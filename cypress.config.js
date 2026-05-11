3241b09dc64c7bae98a016ab500d694455c5533a
ChristianSouza12 <christianlmsouza@outlook.com>
2026-05-11 10:56:57 -0300

    feat: add HTML test reporting with Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,15 @@ const { defineConfig } = require("cypress")
 module.exports = defineConfig({
   allowCypressEnv: true,
 
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+
   e2e: {
     baseUrl: "https://automationpratice.com.br/",
     specPattern: "**/*.feature",
@@ -13,6 +22,8 @@ module.exports = defineConfig({
 
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config)
+
+      require("cypress-mochawesome-reporter/plugin")(on)
 
       on(
         "file:preprocessor",
